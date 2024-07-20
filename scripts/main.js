9598962dc8eef73109ba16777ef53c0f3d3ca6af
Seplaz <57974538+Seplaz@users.noreply.github.com>
2024-07-20 16:31:06 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let phrases = [
   { text: 'Какашка', image: 'https://em-content.zobj.net/source/apple/391/pile-of-poo_1f4a9.png' },
   { text: 'Секси', image: 'https://em-content.zobj.net/source/apple/391/smiling-face-with-horns_1f608.png' },
   { text: 'Милашка', image: 'https://em-content.zobj.net/source/apple/391/smiling-face-with-halo_1f607.png' },
-  { text: 'Алкашка', image: 'https://em-content.zobj.net/source/apple/391/beer-mug_1f37a.png' },
+  { text: 'Алкашка', image: 'https://em-content.zobj.net/source/apple/391/clinking-beer-mugs_1f37b.png' },
   { text: 'Красотка', image: 'https://em-content.zobj.net/source/apple/391/smiling-face-with-heart-eyes_1f60d.png' },
   { text: 'Всё, пока!', image: 'https://em-content.zobj.net/source/apple/391/waving-hand_1f44b.png' }
 ];
