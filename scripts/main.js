6cff707323f27ff68cf5b10846be92008250b47c
Seplaz <57974538+Seplaz@users.noreply.github.com>
2024-08-13 16:58:52 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let phrases = [
-  { text: 'Какашка', image: 'https://em-content.zobj.net/source/apple/391/pile-of-poo_1f4a9.png' },
-  { text: 'Секси', image: 'https://em-content.zobj.net/source/apple/391/smiling-face-with-horns_1f608.png' },
-  { text: 'Милашка', image: 'https://em-content.zobj.net/source/apple/391/smiling-face-with-halo_1f607.png' },
-  { text: 'Алкашка', image: 'https://em-content.zobj.net/source/apple/391/clinking-beer-mugs_1f37b.png' },
-  { text: 'Красотка', image: 'https://em-content.zobj.net/source/apple/391/smiling-face-with-heart-eyes_1f60d.png' },
-  { text: 'Всё, пока!', image: 'https://em-content.zobj.net/source/apple/391/waving-hand_1f44b.png' }
+  { text: 'Какашка', image: 'images/kakashka.png' },
+  { text: 'Арбузерша', image: 'images/arbuzersha.png' },
+  { text: 'Милашка', image: 'images/milashka.png' },
+  { text: 'Алкашка', image: 'images/beer.png' },
+  { text: 'Красотка', image: 'images/krasotka.png' },
+  { text: 'Всё, пока!', image: 'images/poka.png' }
 ];
 
 let phrase = document.querySelector('.phrase');
